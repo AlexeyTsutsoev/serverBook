5ea4db088ad8b9dac4eefbffbe0dfbd98718fa6f
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-01 17:55:34 +0300

    refactor app.js

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-const { response, request } = require("express");
 const express = require("express");
-const fs = require("fs");
-const bodyParser = require("body-parser");
-const jsonParser = express.json();
-
-const users = require("./db/models/users");
-
 const authRouter = require("./routes/auth");
+const corsMiddleWare = require("./middleware/cors");
 
 const app = express();
 
+app.use(corsMiddleWare);
 app.use(express.json());
 app.use("/auth", authRouter);
-
-app.get("/", (request, response) => {
-  console.log(users.name);
-  response.sendFile(__dirname + "/signPage.html");
-});
 
 app.listen(8080, (err) => {
   if (err) {

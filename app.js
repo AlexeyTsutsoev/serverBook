5f57187c7d72004896ff1e44169f694f95214ce3
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-02 14:44:53 +0300

    install cors lib and refactor app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const authRouter = require("./routes/auth");
-const corsMiddleWare = require("./middleware/cors");
+const cors = require("cors");
 
 const app = express();
 
-app.use(corsMiddleWare);
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter);
 

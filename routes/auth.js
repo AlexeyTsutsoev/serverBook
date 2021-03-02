fbf16d799e6a6e19b841f76667b68e7d0a08bf96
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-02 14:47:01 +0300

    add new router

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,12 @@ const express = require("express");
 const authCtrl = require("../controllers/auth");
 const validate = require("../validators");
 const authVal = require("../validators/auth");
+const isAuthenticated = require("../middleware/isAuthorized");
 
 const router = express.Router();
 
 router.post("/sign-up", validate(authVal.signUp), authCtrl.signUp);
 router.post("/sign-in", validate(authVal.signIn), authCtrl.signIn);
-router.get("/me", /* isAuthenticated */ authCtrl.checkUser);
+router.get("/me", isAuthenticated, authCtrl.checkUser);
 
 module.exports = router;

ae9778846ffb620336124e353dbbbfbac7c9b49a
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-04 18:16:21 +0300

    add filter for books

--- controllers/book.js
+++ controllers/book.js
@@ -1,60 +1,81 @@
-const { Sequelize } = require("../db/models");
 const db = require("../db/models");
 
+const getPagination = (page, size) => {
+  const DEFAULT_LIMIT = 6;
+  const limit = size ? +size : DEFAULT_LIMIT;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+};
+
 const makeFilter = (query) => {
-  const filter = {};
+  // &sort=price_asc
+  const filter = {
+    include: [
+      {
+        model: db.authors,
+      },
+      {
+        model: db.publishers,
+      },
+    ],
+    // order: ["id"],
+  };
+
   const where = {};
   if (query.authors) {
-    where.author_id = query.authors;
+    const authorsString = query.authors.split(",");
+    const authorsNumber = authorsString.map((i) => parseInt(i));
+    where.author_id = authorsNumber;
   }
   if (query.publishers) {
-    where.publisher_id = query.publishers;
+    const publishersString = query.publishers.split(",");
+    const publishersNumber = publishersString.map((i) => parseInt(i));
+    where.publisher_id = publishersNumber;
   }
-  if (query.limit) {
-    filter.limit = query.limit;
+  if (query.categories) {
+    const categoriesString = query.categories.split(",");
+    const categoriesNumber = categoriesString.map((i) => parseInt(i));
+    filter.include.push({
+      model: db.categories,
+      required: true,
+      through: {
+        model: db.bookCategories,
+        where: {
+          category_id: categoriesNumber,
+        },
+      },
+    });
   }
 
+  const { limit, offset } = getPagination(query.page, query.limit);
+
   filter.where = where;
+  filter.limit = limit;
+  filter.offset = offset;
   return filter;
 };
 
 const getBooks = async (request, response) => {
   try {
-    // api/books?caterories=[1,2]&authors=[12,13]&page=2&limit=6
     const query = request.query;
     const filter = makeFilter(query);
-    // const filtredBook = await db.books.findAll({
-    //   where: {
-    //     //publisher_id: query.publishers,
-    //     [Op.or]: [{ author_id: query.authors }, { authorId: 4 }],
-    //   },
+
+    const books = await db.books.findAndCountAll(filter);
+    //   {
     //   include: [
     //     {
-    //       model: db.authors,
-    //     },
-    //     {
-    //       model: db.publishers,
+    //       model: db.categories,
+
+    //       through: {
+    //         model: db.bookCategories,
+    //         where: {
+    //           category_id: [1, 3],
+    //         },
+    //       },
     //     },
     //   ],
-    //   // order: ["id"],
-    // });
-
-    const baseFilter = {
-      include: [
-        {
-          model: db.authors,
-        },
-        {
-          model: db.publishers,
-        },
-      ],
-      order: ["id"],
-    };
-
-    const books = await db.books.findAll({
-      ...baseFilter,
-      ...filter,
-    });
+    // }
 
     return response.status(201).json(books);
   } catch (err) {

e91e6f9f3831f5de1bd97bc8ed95ae51330a94f2
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-05 12:32:05 +0300

    refactor jwt

--- utils/index.js
+++ utils/index.js
@@ -1,14 +1,36 @@
 const config = require("../config");
 const jwt = require("jsonwebtoken");
 
+const tokens = config.jwt.tokens;
+const secret = config.jwt.secret;
+
+const createAccessToken = (user) => {
+  const payload = {
+    userId: user.id,
+    type: tokens.access.type,
+  };
+  const lifeTime = { expiresIn: tokens.access.expiresIn };
+
+  return jwt.sign(payload, secret, lifeTime);
+};
+
+const createRefreshToken = (user) => {
+  const payload = {
+    userId: user.id,
+    type: tokens.refresh.type,
+  };
+
+  const lifeTime = { expiresIn: tokens.refresh.expiresIn };
+
+  return jwt.sign(payload, secret, lifeTime);
+};
+
 const createTokensPair = (user) => {
+  const access = createAccessToken(user);
+  const refresh = createRefreshToken(user);
   const result = {
-    accessToken: jwt.sign({ userId: user.id }, config.jwt.secret, {
-      expiresIn: config.jwt.accessTokenExpiresIn,
-    }),
-    refreshToken: jwt.sign({ userId: user.id }, config.jwt.secret, {
-      expiresIn: config.jwt.refreshTokenExpiresIn,
-    }),
+    accessToken: access,
+    refreshToken: refresh,
   };
   return result;
 };

7db94e8233928a6e94b8d50e33889f2ce2363c7b
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-09 18:06:45 +0300

    add api prefix

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const cors = require("cors");
 const authRouter = require("./routes/auth");
 const filtersRouter = require("./routes/filters");
 const bookRouter = require("./routes/book");
+const commentsRouter = require("./routes/comments");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/auth", authRouter);
-app.use("/filters", filtersRouter);
-app.use("/book", bookRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/filters", filtersRouter);
+app.use("/api/book", bookRouter);
+app.use("/api/comments", commentsRouter);
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  res.status(statusCode).json({ message: err.message });
+});
 
 app.listen(8080, (err) => {
   if (err) {

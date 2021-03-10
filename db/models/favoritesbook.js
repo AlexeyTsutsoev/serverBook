b3d7110bc2216587f8c5d97afc867352b57a4ce4
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-10 17:49:51 +0300

    add association

--- db/models/favoritesbook.js
+++ db/models/favoritesbook.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class favoritesbook extends Model {
     /**
@@ -11,14 +9,24 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      favoritesbook.belongsTo(models.books, {
+        foreignKey: "book_id",
+      });
+
+      favoritesbook.belongsTo(models.users, {
+        foreignKey: "user_id",
+      });
     }
-  };
-  favoritesbook.init({
-    book_id: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'favoritesbook',
-  });
+  }
+  favoritesbook.init(
+    {
+      book_id: DataTypes.INTEGER,
+      user_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "favoritesbook",
+    }
+  );
   return favoritesbook;
 };

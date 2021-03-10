5400cf012dac75acfc3ac75de3ce1f12640a2c0e
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-10 17:47:25 +0300

    add favorites router on app

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const authRouter = require("./routes/auth");
 const filtersRouter = require("./routes/filters");
 const bookRouter = require("./routes/book");
 const commentsRouter = require("./routes/comments");
+const favoritesRouter = require("./routes/favorites");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRouter);
 app.use("/api/filters", filtersRouter);
 app.use("/api/book", bookRouter);
 app.use("/api/comments", commentsRouter);
-
+app.use("/api/favorites", favoritesRouter);
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
   res.status(statusCode).json({ message: err.message });

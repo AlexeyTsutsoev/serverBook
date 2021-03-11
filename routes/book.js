65955e3921a417b76e390c76edc2cfcb74946e7f
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-11 17:29:12 +0300

    add create book controller to router

--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bookCtrl = require("../controllers/book");
+const isAdmin = require("../middleware/isAdmin");
 
 const router = express.Router();
 
 router.get("/", bookCtrl.getBooks);
 router.get("/:id", bookCtrl.getOneBook);
+router.use(isAdmin);
+router.post("/add", bookCtrl.addNewBook);
 
 module.exports = router;
